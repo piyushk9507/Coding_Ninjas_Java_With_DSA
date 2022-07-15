fb605fe31a70932b6fff9bcefbe3148839742d8e
Piyush Kumar <40144445+piyushk9507@users.noreply.github.com>
2022-07-15 23:06:08 +0530

    Update Check AB.java

--- Check AB.java
+++ Check AB.java
@@ -1,5 +1,6 @@
 /*
 Suppose you have a string, S, made up of only 'a's and 'b's. Write a recursive function that checks if the string was generated using the following rules:
+
 a. The string begins with an 'a'
 b. Each 'a' is followed by nothing or an 'a' or "bb"
 c. Each "bb" is followed by nothing or an 'a'
